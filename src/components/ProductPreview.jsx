64e6f8656dd5706ac28baa500eca1b1a4fc2209f
alain eloma <alainelomapro@gmail.com>
2023-12-01 11:56:55 +0100

    retrieving product datas from IDs using useParams

---
+++ src/components/ProductPreview.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductPreview(){
+    return(
+        <>
+        <p>Here you should see one Single Product</p>
+        </>
+    )
+}
--- src/pages/singleProduct.jsx
+++ src/pages/singleProduct.jsx
@@ -1,6 +1,14 @@
+import React from "react";
+import ProductPreview from "../components/ProductPreview";
+import { useParams } from "react-router-dom";
+
 function SingleProduct() {
+    const {id} = useParams()
     return(
-        <h1>Single Product</h1>
+        <>
+        <h1>Single Product {id}</h1>
+        <ProductPreview />
+        </>
     )
     
 }
